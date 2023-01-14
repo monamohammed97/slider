2212c5e84c5f80a52a9c6ddc9be80282b092d3ad
Mona Mohammed <mona00mona1997@gmail.com>
2023-01-14 22:38:16 +0200

    finish

---
+++ js/slickSlider.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    // setting-name: setting-value
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  });
+});
